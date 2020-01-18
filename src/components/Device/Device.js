b8e373cc3a691966232c8a91e3a5f026150d4ea8
elenabliznakova <elenab@starcoders.com>
2020-01-18 11:53:41 +0200

    First interaction with the data, set up of package.json required

--- src/components/Device/Device.js
+++ src/components/Device/Device.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {DataView, DataViewLayoutOptions} from 'primereact/dataview';
+
 import './Device.css';
 
 const device = (props) => {
+    const message = props.data.messageType
 	return (
-		<div>device</div>
+		<article className="Device">
+        <div>{props.data.messageType}</div>
+        <div>{props.data.battery}</div>
+
+    </article>
 	)
 }
 export default device;
--- src/containers/Devices/Devices.js
+++ src/containers/Devices/Devices.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import Device from '../../components/Device/Device';
+import axios from 'axios';
 import './Devices.css';
 
 class Devices extends Component {
-
+	constructor() {
+		super();
+		this.state = {
+			devices: [],
+			layout: 'list'
+		};
+	}
+    componentDidMount() {
+		const trails = axios.get("http://localhost:3000")
+            .then(response => {
+                this.setState({devices: response.data})
+                console.log(response)
+            })
+	}
     render () {
-
+		const devices = this.state.devices.map((device) =>
+            <Device data={device.lastData.content} key={device.query.id.value}>{device.lastData.content.battery}</Device>
+		)
+        console.log(this.state.devices[0])
         return (
             <div>
                 <section className="Devices">
-                    List of Devices
+                    {devices}
                 </section>
             </div>
         );
